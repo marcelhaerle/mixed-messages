1025f233c27917b91c3c1c6539f81f474564c691
Marcel Härle <marcel.haerle@sonnvest.de>
2022-01-24 17:21:47 +0100

    Implement random selection

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const fs = require("fs")
 const quotesContent = fs.readFileSync("./quotes.json")
 const quotes = JSON.parse(quotesContent)
 
-console.log(quotes)
+const selectedRandomQuote = () => {
+    const rndIdx = Math.floor(Math.random() * quotes.length)
+    return quotes[rndIdx]
+}
+
+console.log(selectedRandomQuote())
